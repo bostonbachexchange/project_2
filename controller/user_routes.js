be03de2b9d5a3fbf25e18156c822b8be7ab113d8
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-07-18 11:37:13 -0400

    Added home page

--- controller/user_routes.js
+++ controller/user_routes.js
@@ -39,14 +39,16 @@ router.post('/login', async (req, res) => {
                     console.log('this is the session after login', req.session)
                     ////// checks to see if profile exist /////////
                     const profile = await Profile.find({"username": username});
-                    if (profile) {
+                    if (profile.length !== 0) {
                         //console.log(profile)
                         console.log("username", username)
+                        
                         console.log('profile exist', profile)
                         res.redirect('/assignments')
+                        //res.redirect('assignments/home')
                     } 
-                     else { console.log('no profile exist')
-                     console.log('no profile exist')
+                     else { console.log('no profile exist', profile)
+                     console.log('profile length', profile.length)
                      res.redirect('/profile/new')
                     }
                 } else {
@@ -65,34 +67,38 @@ router.post('/login', async (req, res) => {
 router.get('/login', (req, res) => {
     res.render('users/login')
 })
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body
-    User.findOne({ username })
-        .then(async (user) => {
-            if (user) {
-                const result = await bcrypt.compare(password, user.password)
 
-                if (result) {
-                    req.secure.username = username
-                    req.session.loggedIn = true
-                    req.session.userId = user._id
-                    console.log('this is the session after login', req.session)
-                    res.redirect('/assignments.index')
-                } else {
-                    res.json({ error: 'username or password incorrect'})
-                }
-            } else {
-                res.json({ error: 'user does not exist' })
-            }
-        })
-        // if they don't we'll redirect to the sign up page
-        .catch(error => {
-            console.log(error)
-            res.json(error)
-        })
-})
+// router.post('/login', async (req, res) => {
+//     const { username, password } = req.body
+//     User.findOne({ username })
+//         .then(async (user) => {
+//             if (user) {
+//                 const result = await bcrypt.compare(password, user.password)
+
+//                 if (result) {
+//                     req.secure.username = username
+//                     req.session.loggedIn = true
+//                     req.session.userId = user._id
+//                     console.log('this is the session after login', req.session)
+//                     // res.redirect('/assignments.index')
+//                     res.render('/assignment/home')
+//                 } else {
+//                     res.json({ error: 'username or password incorrect'})
+//                 }
+//             } else {
+//                 res.json({ error: 'user does not exist' })
+//             }
+//         })
+//         // if they don't we'll redirect to the sign up page
+//         .catch(error => {
+//             console.log(error)
+//             res.json(error)
+//         })
+// })
 ///logout
 router.get('/logout', (req, res) => {
+    ////// check if loggin in false, go to login page////
+
     req.session.destroy(ret => {
         console.log('this is returned from req.session.destroy', ret)
         res.redirect('/users/login')
